3edec0940e2a0c1ec7aeac9881c249e70a163e7d
Mokh <52209210+M0kh@users.noreply.github.com>
2024-08-20 17:00:18 +0300

    Update Tor-IP-Addresses.py

--- Tor-IP-Addresses.py
+++ Tor-IP-Addresses.py
@@ -2,16 +2,25 @@ import requests
 import re
 import os
 
-url = 'https://check.torproject.org/exit-addresses'
-req = requests.get(url, allow_redirects=True)
-open('Tor.txt', 'wb').write(req.content)
+try:
+    print("Downloading Tor exit addresses list...")
+    url = 'https://check.torproject.org/exit-addresses'
+    req = requests.get(url, allow_redirects=True)
+    open('Tor.txt', 'wb').write(req.content)
+except requests.exceptions.ConnectTimeout:
+    print("Failed to download the exit addresses list due to a timeout error, please use a VPN.")
 
-with open('Tor.txt') as f:
-    for line in f:
-        match = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
-        if match:
-            torexit = open('Tor-IP-Addresses.txt', 'a')
-            torexit.write(match.group() + '\n')
-            torexit.close()
-
-os.remove("Tor.txt")
+try:
+    with open('Tor.txt') as f:
+        print("Extracting IP Addresses...")
+        for line in f:
+            match = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
+            if match:
+                torexit = open('Tor-IP-Addresses.txt', 'a')
+                torexit.write(match.group() + '\n')
+                torexit.close()
+    print("Done!")
+    os.remove("Tor.txt")
+except FileNotFoundError:
+    print("")
+    
